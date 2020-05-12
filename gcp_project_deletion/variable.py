06012cc7f0deccbc0caa002f7797510a19c61b42
Pochu-Sandeep <63863569+Pochu-Sandeep@users.noreply.github.com>
2020-05-11 18:32:36 -0700

    Update variable.py

--- gcp_project_deletion/variable.py
+++ gcp_project_deletion/variable.py
@@ -1,13 +1,8 @@
-import os
-
 import sys
 
 from google.oauth2 import service_account
 
 from googleapiclient import discovery
-
-#from google.cloud import storage
-#os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = sys.argv[1]
 
 credentials = service_account.Credentials.from_service_account_file(sys.argv[1])
 
@@ -17,4 +12,4 @@ resource_manager_service = discovery.build('cloudresourcemanager', 'v1', credent
 
 service_usage_service = discovery.build('serviceusage', 'v1', credentials=credentials)
 
-#storage_client = storage.Client.from_service_account_json("/home/sandeep_pochu/sap-coe-devops-test.json")
+storage_client = storage.Client.from_service_account_json(sys.argv[1])
