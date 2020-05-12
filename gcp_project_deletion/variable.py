66533de9dd7163007dc1794e950dd4b2fe3669ad
Pochu-Sandeep <63863569+Pochu-Sandeep@users.noreply.github.com>
2020-05-11 19:26:47 -0700

    Update variable.py

--- gcp_project_deletion/variable.py
+++ gcp_project_deletion/variable.py
@@ -4,6 +4,8 @@ from google.oauth2 import service_account
 
 from googleapiclient import discovery
 
+from google.cloud import storage
+
 credentials = service_account.Credentials.from_service_account_file(sys.argv[1])
 
 service = discovery.build('compute', 'v1', credentials=credentials)
@@ -12,4 +14,4 @@ resource_manager_service = discovery.build('cloudresourcemanager', 'v1', credent
 
 service_usage_service = discovery.build('serviceusage', 'v1', credentials=credentials)
 
-#storage_client = storage.Client.from_service_account_json(sys.argv[1])
+storage_client = storage.Client.from_service_account_json(sys.argv[1])
