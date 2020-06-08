d04b30c220ceb32f2ef7f58bfb471a9d45e745b8
Pochu-Sandeep <63863569+Pochu-Sandeep@users.noreply.github.com>
2020-06-08 12:25:57 -0700

    Update variable.py

--- gcp_project_deletion/variable.py
+++ gcp_project_deletion/variable.py
@@ -12,8 +12,6 @@ service = discovery.build('compute', 'v1', credentials=credentials)
 
 resource_manager_service = discovery.build('cloudresourcemanager', 'v1', credentials=credentials)
 
-resource_manager_service_project = discovery.build('cloudresourcemanager', 'v1', credentials=credentials)
-
 service_usage_service = discovery.build('serviceusage', 'v1', credentials=credentials)
 
 storage_client = storage.Client.from_service_account_json(sys.argv[1])
