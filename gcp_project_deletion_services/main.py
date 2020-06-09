b55d6871f72c91fb0ba75db28a8fd95d08e9d176
Pochu-Sandeep <63863569+Pochu-Sandeep@users.noreply.github.com>
2020-06-08 18:45:53 -0700

    Update main.py

--- gcp_project_deletion_services/main.py
+++ gcp_project_deletion_services/main.py
@@ -16,37 +16,37 @@ class main:
 
                 print("Checking for VM's, Disks, VPN and Firewalls in project " + project_id)
 
-                from gcp_project_deletion.compute import compute
+                from gcp_project_deletion_services.compute import compute
 
                 obj_compute = compute()
 
                 instance_exist = obj_compute.vm_list(project_id)
 
-                from gcp_project_deletion.disks import disks
+                from gcp_project_deletion_services.disks import disks
 
                 obj_disks = disks()
 
                 disks_exist = obj_disks.disks_list(project_id)
 
-                from gcp_project_deletion.firewall import firewall
+                from gcp_project_deletion_services.firewall import firewall
 
                 obj_firewall = firewall()
 
                 firewall_exist = obj_firewall.firewall_list(project_id)
 
-                from gcp_project_deletion.vpn import vpn
+                from gcp_project_deletion_services.vpn import vpn
 
                 obj_vpn = vpn()
 
                 vpn_exist = obj_vpn.vpn_list(project_id)
 
-                from gcp_project_deletion.storage import storage
+                from gcp_project_deletion_services.storage import storage
 
                 obj_storage = storage()
 
                 storage_exist = obj_storage.storage_list(project_id)
 
-                from gcp_project_deletion.liens import liens
+                from gcp_project_deletion_services.liens import liens
 
                 obj_liens = liens()
 
@@ -64,7 +64,7 @@ class main:
 
                 else:
 
-                        from gcp_project_deletion.variable import resource_manager_service
+                        from gcp_project_deletion_services.variable import resource_manager_service
 
                         request = resource_manager_service.projects().delete(project_Id=project_id)
 
@@ -76,7 +76,7 @@ class main:
 
                 project_id = projects
 
-                from gcp_project_deletion.variable import resource_manager_service
+                from gcp_project_deletion_services.variable import resource_manager_service
 
                 project_status_request = resource_manager_service.projects().get(projectId=project_id)
 
