97c17d461401a56600b3cf68aaefac552e4a2107
Pochu-Sandeep <63863569+Pochu-Sandeep@users.noreply.github.com>
2020-06-08 18:35:56 -0700

    Update main.py

--- gcp_project_deletion_services/main.py
+++ gcp_project_deletion_services/main.py
@@ -1,3 +1,7 @@
+import os
+
+import sys
+
 class main:
 
     def services_exist(self):
@@ -8,65 +12,85 @@ class main:
 
         for projects in projects_list:
 
-            project_id = projects
+                project_id = projects
 
-            print("Checking for VM's, Disks, VPN and Firewalls in project " + project_id)
+                print("Checking for VM's, Disks, VPN and Firewalls in project " + project_id)
 
-            from gcp_project_deletion_services.compute import compute
+                from gcp_project_deletion.compute import compute
 
-            obj_compute = compute()
+                obj_compute = compute()
 
-            instance_exist = obj_compute.vm_list(project_id)
+                instance_exist = obj_compute.vm_list(project_id)
 
-            from gcp_project_deletion_services.disks import disks
+                from gcp_project_deletion.disks import disks
 
-            obj_disk = disks()
+                obj_disks = disks()
 
-            disks_exist = obj_disk.disks_list(project_id)
+                disks_exist = obj_disks.disks_list(project_id)
 
-            from gcp_project_deletion.firewall import firewall
+                from gcp_project_deletion.firewall import firewall
 
-            obj_firewall = firewall()
+                obj_firewall = firewall()
 
-            firewall_exist = obj_firewall.firewall_list(project_id)
+                firewall_exist = obj_firewall.firewall_list(project_id)
 
-            from gcp_project_deletion.vpn import vpn
+                from gcp_project_deletion.vpn import vpn
 
-            obj_vpn = vpn()
+                obj_vpn = vpn()
 
-            vpn_exist = obj_vpn.vpn_list(project_id)
+                vpn_exist = obj_vpn.vpn_list(project_id)
 
-            from gcp_project_deletion.storage import storage
+                from gcp_project_deletion.storage import storage
 
-            obj_storage = storage()
+                obj_storage = storage()
 
-            storage_exist = obj_storage.storage_list(project_id)
+                storage_exist = obj_storage.storage_list(project_id)
 
-            from gcp_project_deletion.liens import liens
+                from gcp_project_deletion.liens import liens
 
-            obj_liens = liens()
+                obj_liens = liens()
 
-            liens_exist = obj_liens.liens_list(project_id)
+                liens_exist = obj_liens.liens_list(project_id)
 
-            from gcp_project_deletion.endpoint import endpoint
+                    #from gcp_project_deletion.endpoint import endpoint
 
-            obj_endpoint = endpoint()
+                    #obj_endpoint = endpoint()
 
-            endpoint_exist = obj_endpoint.endpoint_list(project_id)
+                    #endpoint_exist = obj_endpoint.endpoint_list(project_id)
 
-            if (instance_exist | disks_exist | firewall_exist | vpn_exist | liens_exist | endpoint_exist):
+                if (instance_exist | disks_exist | firewall_exist | vpn_exist | storage_exist | liens_exist):
 
-                print("vm exists in Project "+project_id+" and it will not  be deleted")
+                        print("Project "+project_id+" will not  be deleted")
 
-            else:
+                else:
+
+                        from gcp_project_deletion.variable import resource_manager_service
+
+                        request = resource_manager_service.projects().delete(project_Id=project_id)
+
+                        request.execute()
+
+                        print("Project "+project_id+" is shutting down")
+
+        for projects in projects_list:
+
+                project_id = projects
 
                 from gcp_project_deletion.variable import resource_manager_service
 
-                request = resource_manager_service.projects().delete(projectId=project_id)
+                project_status_request = resource_manager_service.projects().get(projectId=project_id)
 
-                request.execute()
+                project_status_response = project_status_request.execute()
 
-                print("Project "+project_id+" is now shutdown")
+                project_status = project_status_response.get('lifecycleState')
+
+                if project_status == 'ACTIVE':
+
+                        sys.exit(-1)
+
+                else:
+
+                        print(project_id+" is now deleted")
 
 obj_main = main()
 
