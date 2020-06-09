af7190186caf5f9197bec23b5657f60b97cfa067
Pochu-Sandeep <63863569+Pochu-Sandeep@users.noreply.github.com>
2020-06-08 19:01:19 -0700

    Update storage.py

--- gcp_project_deletion_services/storage.py
+++ gcp_project_deletion_services/storage.py
@@ -2,7 +2,7 @@ class storage:
 
     def storage_list(self,project_id):
 
-        from gcp_project_deletion.variable import storage_client
+        from gcp_project_deletion_services.variable import storage_client
 
         storage_list = list(storage_client.list_buckets(project=project_id))
 
