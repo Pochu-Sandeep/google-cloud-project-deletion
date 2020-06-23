14a7932a645c6c32fcf1f0989621afca7a7125c7
Pochu-Sandeep <63863569+Pochu-Sandeep@users.noreply.github.com>
2020-06-22 21:44:34 -0700

    Update storage.py

--- gcp_project_deletion_services/storage.py
+++ gcp_project_deletion_services/storage.py
@@ -1,8 +1,33 @@
 class storage:
 
     def storage_list(self,project_id):
+        
+        from gcp_project_deletion_services.variable import resource_manager_service
 
-        from gcp_project_deletion.variable import storage_client
+        get_project_request = resource_manager_service.projects().get(projectId=project_id)
+
+        get_project_response = get_project_request.execute()
+
+        project_number = get_project_response.get('projectNumber')
+
+        storage_api = "storage-component.googleapis.com"
+
+        from gcp_project_deletion_services.variable import service_usage_service
+
+        request = service_usage_service.services().get(
+            name="projects/" + project_number + "/services/" + storage_api)
+
+        response = request.execute()
+
+        storage_api_status = response.get('state')
+
+        if storage_api_status == 'DISABLED':
+
+            endpoint_enable_request = service_usage_service.services().enable(name="projects/" + project_number + "/services/" + storage_api)
+
+            endpoint_enable_response = endpoint_enable_request.execute()
+
+        from gcp_project_deletion_services.variable import storage_client
 
         storage_list = list(storage_client.list_buckets(project=project_id))
 
