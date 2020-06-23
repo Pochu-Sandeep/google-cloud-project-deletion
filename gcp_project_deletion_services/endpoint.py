c3d44b797e22babb225d3c2abc8a6815c8e38b39
Pochu-Sandeep <63863569+Pochu-Sandeep@users.noreply.github.com>
2020-06-22 21:52:00 -0700

    Update endpoint.py

--- gcp_project_deletion_services/endpoint.py
+++ gcp_project_deletion_services/endpoint.py
@@ -1,6 +1,30 @@
 class endpoint:
 
     def endpoint_list(self, project_id):
+        
+        from gcp_project_deletion_services.variable import resource_manager_service
+
+        get_project_request = resource_manager_service.projects().get(projectId=project_id)
+
+        get_project_response = get_project_request.execute()
+
+        project_number = get_project_response.get('projectNumber')
+
+        service_usage_api = "serviceusage.googleapis.com"
+
+        from gcp_project_deletion_services.variable import service_usage_service
+
+        request = service_usage_service.services().get(name="projects/" + project_number + "/services/" + service_usage_api)
+
+        response = request.execute()
+
+        storage_api_status = response.get('state')
+
+        if storage_api_status == 'DISABLED':
+            
+            endpoint_enable_request = service_usage_service.services().enable(name="projects/" + project_number + "/services/" + service_usage_api)
+
+            endpoint_enable_response = endpoint_enable_request.execute()
 
         from gcp_project_deletion_services.variable import service_usage_service
 
