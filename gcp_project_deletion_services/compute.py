d55934fd9458de1d84c2b864ae9fbfbea77db4c3
Pochu-Sandeep <63863569+Pochu-Sandeep@users.noreply.github.com>
2020-07-07 09:25:27 -0700

    Update compute.py

--- gcp_project_deletion_services/compute.py
+++ gcp_project_deletion_services/compute.py
@@ -1,6 +1,36 @@
 class compute:
 
     def vm_list(self, project_id):
+        
+        from gcp_project_deletion_services.variable import resource_manager_service
+
+        get_project_request = resource_manager_service.projects().get(projectId=project_id)
+
+        get_project_response = get_project_request.execute()
+
+        project_number = get_project_response.get('projectNumber')
+
+        compute_engine_api = "compute.googleapis.com"
+
+        from gcp_project_deletion_services.variable import service_usage_service
+
+        request = service_usage_service.services().get(name="projects/" + project_number + "/services/" + compute_engine_api)
+
+        response = request.execute()
+
+        compute_engine_api_status = response.get('state')
+
+        print(compute_engine_api_status)
+
+        if compute_engine_api_status == 'DISABLED':
+
+            endpoint_enable_request = service_usage_service.services().enable(name="projects/" + project_number + "/services/" + compute_engine_api)
+
+            endpoint_enable_response = endpoint_enable_request.execute()
+
+            print(endpoint_enable_response)
+
+            time.sleep(600)
 
         from gcp_project_deletion_services.variable import service
 
